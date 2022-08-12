881f46c309fde77bb4ec627895c7b1dbd06393f4
stevensamir20 <steven.samir@eng.suez.edu.eg>
2022-08-13 00:53:55 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { Buttons } from './buttonsData';
+import {useState} from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ['x','/','+','-','.'];
+
+  const updateCalc = value => {
+    if ((operators.includes(value) && calc === '') || 
+        (operators.includes(value) && operators.includes(calc.slice(-1)))  
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+    if (!operators.includes(value)){
+      setResult(eval(calc + value).toString());
+    }
+  }
+
+  const calcualte = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const reset = () => {
+    setCalc("");
+    setResult("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='container'>
+        <div id='forumla-screen'>{result ? <span>{result}</span> : ""}</div>
+        <div id='output-screen'>{calc || "0"}</div>
+        <div id='input-screen'>
+          <div className='clear'>
+              <button id='clear' className='reset-button' onClick={() => reset()}>AC</button>
+          </div>
+          <div className='equal'>
+            <button id='equal' className='equal-button' onClick={() => calcualte()}>=</button>
+          </div>
+          {Buttons.map((item, id)=> {
+            return (
+            <div key={id} className={item.idName}>
+              <button id={item.idName} className={item.cName} onClick={() => updateCalc(`${item.title}`)}>{item.title}</button>
+            </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/buttonsData.js
@@ -0,0 +1,78 @@
+export const Buttons = [
+  
+  {
+    title: '/',
+    idName: 'divide',
+    cName: 'operators'
+  },
+  {
+    title: 'x',
+    idName: 'multiply',
+    cName: 'operators'
+  },
+  {
+    title: '7',
+    idName: 'seven',
+    cName: 'digits'
+  },
+  {
+    title: '8',
+    idName: 'eight',
+    cName: 'digits'
+  },
+  {
+    title: '9',
+    idName: 'nine',
+    cName: 'digits'
+  },
+  {
+    title: '-',
+    idName: 'subtract',
+    cName: 'operators'
+  },
+  {
+    title: '4',
+    idName: 'four',
+    cName: 'digits'
+  },
+  {
+    title: '5',
+    idName: 'five',
+    cName: 'digits'
+  },
+  {
+    title: '6',
+    idName: 'six',
+    cName: 'digits'
+  },
+  {
+    title: '+',
+    idName: 'add',
+    cName: 'operators'
+  },
+  {
+    title: '1',
+    idName: 'one',
+    cName: 'digits'
+  },
+  {
+    title: '2',
+    idName: 'two',
+    cName: 'digits'
+  },
+  {
+    title: '3',
+    idName: 'three',
+    cName: 'digits'
+  },
+  {
+    title: '0',
+    idName: 'zero',
+    cName: 'digits'
+  },
+  {
+    title: '.',
+    idName: 'dot',
+    cName: 'digits'
+  }
+]
